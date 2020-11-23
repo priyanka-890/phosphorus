278fa96390505e890d5d7013ec75a36952c7b3a5
priyanka-890 <priyankaraghav890@gmail.com>
2020-11-24 00:21:46 +0530

    dynamic search

--- script/index.js
+++ script/index.js
@@ -1,1 +1,19 @@
-// document.style.
+function searchBar(){
+var search= document.getElementById("search-div")
+search.style.display="block"
+var nav=document.getElementsByClassName("nav-bar")
+nav[0].style.display="none"
+ 
+}
+
+var searchAnimation= document.getElementById("search")
+searchAnimation.addEventListener('click',searchBar)
+
+function closeBar(){
+    var search= document.getElementById("search-div")
+search.style.display="none"
+var nav=document.getElementsByClassName("nav-bar")
+nav[0].style.display="flex"
+}
+var closeDiv= document.getElementById("close")
+closeDiv.addEventListener('click',closeBar)
