780dbebda2438d9b7fb9891420d3e2aa34cf2619
priyanka-890 <priyankaraghav890@gmail.com>
2020-11-25 17:41:17 +0530

    pricing part of js

---
+++ script/pricing.js
@@ -0,0 +1,64 @@
+// java script for search bar
+// function searchBar() {
+//     var search = document.getElementById("search-div")
+//     search.style.display = "block"
+//     var nav = document.getElementsByClassName("nav-bar")
+//     nav[0].style.display = "none"
+
+// }
+
+// var searchAnimation = document.getElementById("search")
+//     // searchAnimation.addEventListener('click',searchBar)
+
+// function closeBar() {
+//     var search = document.getElementById("search-div")
+//     search.style.display = "none"
+//     var nav = document.getElementsByClassName("nav-bar")
+//     nav[0].style.display = "flex"
+// }
+// var closeDiv = document.getElementById("close")
+// closeDiv.addEventListener('click', closeBar)
+
+// java script for nav-products
+
+function productBar(index) {
+    var productDetails = document.getElementsByClassName("product-div")
+    var displayProp = productDetails[index].style.display
+    productDetails[0].style.display = "none"
+    productDetails[1].style.display = "none"
+    productDetails[2].style.display = "none"
+    if (displayProp == "none") {
+        productDetails[index].style.display = "block"
+    } else {
+        productDetails[index].style.display = "none"
+    }
+
+}
+var product = document.getElementById("products");
+product.addEventListener('click', () => productBar(0));
+var solutions = document.getElementById("solutions");
+solutions.addEventListener('click', () => productBar(1));
+var resources = document.getElementById("resources");
+resources.addEventListener('click', () => productBar(2));
+
+
+// anchor list display
+var anchor1 = document.getElementById("anchor-tab1")
+anchor1.addEventListener("click", openBusiness)
+
+function openBusiness() {
+    var businessDiv = document.getElementById("business-tab")
+    businessDiv.style.display = "block";
+    var personalDiv = document.getElementById("personal-tab")
+    personalDiv.style.display = "none"
+}
+
+var anchor2 = document.getElementById("anchor-tab2")
+anchor2.addEventListener("click", openPersonal)
+
+function openPersonal() {
+    var personalDiv = document.getElementById("personal-tab")
+    personalDiv.style.display = "block";
+    var businessDiv = document.getElementById("business-tab")
+    businessDiv.style.display = "none"
+}
